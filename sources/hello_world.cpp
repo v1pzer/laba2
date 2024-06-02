19fae5245e8ca83a6d40fc410af099cfa9800b4b
Andrey <144845479+v1pzer@users.noreply.github.com>
2024-06-02 13:06:46 +0300

    Conflict

--- sources/hello_world.cpp
+++ sources/hello_world.cpp
@@ -2,8 +2,8 @@
 #include <string>
 
 int main() {
-    std::string name;
-    std::cin >> name;
-    std::cout << std::endl << "Hello world from " << name << std::endl; // Basic string usage
+    std::string name2;
+    std::cin >> name2;
+    std::cout << std::endl << "Hello world from " << name2 << std::endl; // New string usage for conflict
     return 0;
 }
